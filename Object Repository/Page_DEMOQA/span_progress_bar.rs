d70783fe82c4855b35545bbcffb94f01722970e2
Jaher rojas <jaherrojas@gmail.com>
2025-02-23 23:24:55 -0500

    Ultimo commit

--- Object Repository/Page_DEMOQA/span_progress_bar.rs
+++ Object Repository/Page_DEMOQA/span_progress_bar.rs
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>span_Progress Bar</name>
+   <name>span_progress_bar</name>
    <tag></tag>
    <elementGuidId>5bc05d33-278d-4a92-952f-76b48ebd1064</elementGuidId>
    <selectorCollection>
@@ -10,11 +10,15 @@
          <value>div.element-list.collapse.show > ul.menu-list > #item-4 > span.text</value>
       </entry>
       <entry>
+         <key>BASIC</key>
+         <value>//span[(text() = 'Progress Bar' or . = 'Progress Bar')]</value>
+      </entry>
+      <entry>
          <key>XPATH</key>
          <value>(//li[@id='item-4']/span)[3]</value>
       </entry>
    </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
+   <selectorMethod>BASIC</selectorMethod>
    <smartLocatorCollection>
       <entry>
          <key>SMART_LOCATOR</key>
@@ -24,12 +28,12 @@
    <smartLocatorEnabled>false</smartLocatorEnabled>
    <useRalativeImagePath>true</useRalativeImagePath>
    <webElementProperties>
-      <isSelected>false</isSelected>
+      <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>tag</name>
       <type>Main</type>
       <value>span</value>
-      <webElementGuid>cd527345-acfb-4ea8-b8aa-65bd183a97f4</webElementGuid>
+      <webElementGuid>1f5ca9e8-d6d7-447f-8456-20b44b5dcedf</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -37,7 +41,7 @@
       <name>class</name>
       <type>Main</type>
       <value>text</value>
-      <webElementGuid>40d79756-f3ba-4633-a207-89d56b0b538d</webElementGuid>
+      <webElementGuid>4f7a57ce-ab46-488c-bdcc-fe7ee7ceebe0</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -45,7 +49,7 @@
       <name>text</name>
       <type>Main</type>
       <value>Progress Bar</value>
-      <webElementGuid>ae5b10d3-aa13-4432-ba5a-b19b02086c42</webElementGuid>
+      <webElementGuid>9df769f2-9b76-4bc9-87f2-4f08037c26ce</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -53,7 +57,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>id(&quot;app&quot;)/div[@class=&quot;body-height&quot;]/div[@class=&quot;container playgound-body&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-12 mt-4  col-md-3&quot;]/div[@class=&quot;left-pannel&quot;]/div[@class=&quot;accordion&quot;]/div[@class=&quot;element-group&quot;]/div[@class=&quot;element-list collapse show&quot;]/ul[@class=&quot;menu-list&quot;]/li[@id=&quot;item-4&quot;]/span[@class=&quot;text&quot;]</value>
-      <webElementGuid>0dc1007f-bd25-45ab-814f-c40a291da676</webElementGuid>
+      <webElementGuid>e7ce96c1-e99f-4ffe-9eb8-8ebe29eaa996</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -61,7 +65,7 @@
       <name>xpath:idRelative</name>
       <type>Main</type>
       <value>(//li[@id='item-4']/span)[3]</value>
-      <webElementGuid>e41b2b32-325e-4d30-8881-ce5dad4acafe</webElementGuid>
+      <webElementGuid>8d242cc9-1d11-4cb3-ac80-4ab85df95cc1</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -69,7 +73,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Slider'])[1]/following::span[1]</value>
-      <webElementGuid>a0e74bf8-a665-4550-a20c-86175d78a348</webElementGuid>
+      <webElementGuid>b88453e8-5ad5-4444-9464-f5281412cead</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -77,7 +81,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Date Picker'])[1]/following::span[2]</value>
-      <webElementGuid>cf637db1-29ca-43e3-aa1b-972d0ba2066c</webElementGuid>
+      <webElementGuid>34b9349b-c19b-4442-9053-5764fc7af1bb</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -85,7 +89,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Tabs'])[1]/preceding::span[1]</value>
-      <webElementGuid>5b35b6db-37d0-4d66-9e69-0ef8b0192a8e</webElementGuid>
+      <webElementGuid>dc187551-3102-4d7f-a8a6-2a385807b2b2</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -93,7 +97,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Tool Tips'])[1]/preceding::span[2]</value>
-      <webElementGuid>0fb7d9f2-61f5-46fa-88a4-8ca11af0a86b</webElementGuid>
+      <webElementGuid>0aa0f995-bf0c-4eb1-9104-94e9bbe3bce7</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -101,7 +105,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>//*/text()[normalize-space(.)='Progress Bar']/parent::*</value>
-      <webElementGuid>66ae13bd-a476-409e-8133-29f5ce7a2e28</webElementGuid>
+      <webElementGuid>2dd5f213-0b37-45c0-ad7c-76b419c2eaaa</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -109,7 +113,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//div[4]/div/ul/li[5]/span</value>
-      <webElementGuid>89d85cf5-7f13-4cc7-9b74-713e5ecc5706</webElementGuid>
+      <webElementGuid>70260f80-9d11-4abd-8f48-a5103848f1c8</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -117,6 +121,6 @@
       <name>xpath:customAttributes</name>
       <type>Main</type>
       <value>//span[(text() = 'Progress Bar' or . = 'Progress Bar')]</value>
-      <webElementGuid>2dc6689a-dbbf-4d14-b5e3-96ffefc698cc</webElementGuid>
+      <webElementGuid>dcbd6b0b-8c26-4f50-a66b-713b2e7b4f19</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
