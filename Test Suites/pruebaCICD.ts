722bf365b47106824a3e9d6921fb1c6faa0573f2
Jaher rojas <jaherrojas@gmail.com>
2025-02-23 22:29:02 -0500

    Se agraga suit

---
+++ Test Suites/pruebaCICD.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>pruebaCICD</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>000b8458-a8da-4aff-9638-14b61535668a</testSuiteGuid>
+   <testCaseLink>
+      <guid>28019ebb-b0ed-431b-985f-75f46e287751</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/barraProgreso</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
