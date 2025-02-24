cafa0097cd376e3e07c202e6c42117e28cbfdc99
Jaher rojas <jaherrojas@gmail.com>
2025-02-23 21:53:46 -0500

    Commit final barraProgreso

---
+++ Object Repository/Page_DEMOQA/span_Progress Bar.rs
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Progress Bar</name>
+   <tag></tag>
+   <elementGuidId>5bc05d33-278d-4a92-952f-76b48ebd1064</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.element-list.collapse.show > ul.menu-list > #item-4 > span.text</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//li[@id='item-4']/span)[3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:text=&quot;Progress Bar&quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>cd527345-acfb-4ea8-b8aa-65bd183a97f4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>text</value>
+      <webElementGuid>40d79756-f3ba-4633-a207-89d56b0b538d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Progress Bar</value>
+      <webElementGuid>ae5b10d3-aa13-4432-ba5a-b19b02086c42</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;app&quot;)/div[@class=&quot;body-height&quot;]/div[@class=&quot;container playgound-body&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-12 mt-4  col-md-3&quot;]/div[@class=&quot;left-pannel&quot;]/div[@class=&quot;accordion&quot;]/div[@class=&quot;element-group&quot;]/div[@class=&quot;element-list collapse show&quot;]/ul[@class=&quot;menu-list&quot;]/li[@id=&quot;item-4&quot;]/span[@class=&quot;text&quot;]</value>
+      <webElementGuid>0dc1007f-bd25-45ab-814f-c40a291da676</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>(//li[@id='item-4']/span)[3]</value>
+      <webElementGuid>e41b2b32-325e-4d30-8881-ce5dad4acafe</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Slider'])[1]/following::span[1]</value>
+      <webElementGuid>a0e74bf8-a665-4550-a20c-86175d78a348</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Date Picker'])[1]/following::span[2]</value>
+      <webElementGuid>cf637db1-29ca-43e3-aa1b-972d0ba2066c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Tabs'])[1]/preceding::span[1]</value>
+      <webElementGuid>5b35b6db-37d0-4d66-9e69-0ef8b0192a8e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Tool Tips'])[1]/preceding::span[2]</value>
+      <webElementGuid>0fb7d9f2-61f5-46fa-88a4-8ca11af0a86b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Progress Bar']/parent::*</value>
+      <webElementGuid>66ae13bd-a476-409e-8133-29f5ce7a2e28</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[4]/div/ul/li[5]/span</value>
+      <webElementGuid>89d85cf5-7f13-4cc7-9b74-713e5ecc5706</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'Progress Bar' or . = 'Progress Bar')]</value>
+      <webElementGuid>2dc6689a-dbbf-4d14-b5e3-96ffefc698cc</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/barraProgreso/Script1738803055140.groovy
@@ -0,0 +1,53 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+// Abrir el navegador y navegar al sitio web
+WebUI.openBrowser('https://demoqa.com/automation-practice-form')
+
+// Navegar a la sección "Widgets" y luego a "Progress Bar"
+WebUI.click(findTestObject('Object Repository/Page_DEMOQA/div_Widgets'))
+WebUI.click(findTestObject('Object Repository/Page_DEMOQA/span_Progress Bar'))
+
+// Hacer clic en el botón "Start" para iniciar la carga de la barra
+WebUI.click(findTestObject('Object Repository/Page_DEMOQA/button_Start'))
+
+// Monitorear la barra de progreso hasta que alcance el 39%
+def targetPercentage = 39
+def currentPercentage = 0
+
+while (true) {
+    // Obtener el valor del atributo 'aria-valuenow'
+    def progressValue = WebUI.getAttribute(findTestObject('Object Repository/Page_DEMOQA/progressBar'), 'aria-valuenow')
+   
+    // Convertir el valor a entero
+    currentPercentage = progressValue.toInteger()
+    
+    // Detener el bucle si el progreso alcanza o supera el 39%
+    if (currentPercentage >= targetPercentage) {
+        // Presionar el botón "Stop" inmediatamente
+        WebUI.click(findTestObject('Object Repository/Page_DEMOQA/button_Stop'))
+        break
+    }
+    
+    // Esperar un breve momento antes de verificar nuevamente (reducido a 5 ms)
+    WebUI.delay(0.005) // Tiempo mínimo para evitar sobrecargar el sistema
+}
+
+// Cerrar el navegador
+WebUI.closeBrowser()
